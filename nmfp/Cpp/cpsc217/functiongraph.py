2a6c82fa406af557ed2377daeab06515e34d41da
Edward Brash <brash@jlab.org>
2019-10-16 21:28:04 -0400

    Updates

--- nmfp/Cpp/cpsc217/functiongraph.py
+++ nmfp/Cpp/cpsc217/functiongraph.py
@@ -1,45 +1,77 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 from math import *
 from SimpleGraphics import *
 
-ox = 400
-oy = 300
+
+# In[2]:
+
+
+# Set constants
+xcs = 0
+ycs = 0
+xce = 800
+yce = 600
 dx = 30
 dy = 30
+ticksize = 4
+label_offset = 12
+step = 1
+
+# Determine origin position
+ox = (xce-xcs)/2
+oy = (yce-ycs)/2
+
+# Determine minimum and maximum x- and y-axis limits
+xmin = -int(ox/dx)
+xmax = -xmin+1
+ymin = -int(oy/dy)
+ymax = -ymin+1
 
 # Change the background white
 setOutline("white")
 background("white")
 
+
+# In[3]:
+
+
 # Draw the Axes
 
 # Draw lines for axes
 setOutline("black")
-line(0,300,800,300)
-line(400,0,400,600)
+line(xcs,oy,xce,oy)
+line(ox,ycs,ox,yce)
 
 # Draw x-tick marks and x-labels
-for i in range(-13,14):
+for i in range(xmin,xmax):
     xval = ox+i*dx
-    ystart = oy-4
-    yend = oy+4
+    ystart = oy-ticksize
+    yend = oy+ticksize
     line(xval,ystart,xval,yend)
     xlabel = str(i)
-    ytxt = oy+12
+    ytxt = oy+label_offset
     if i!=0:
         text(xval,ytxt,xlabel)
         
 # Draw y-tick marks and y-labels
-for i in range(-10,11):
-    xstart = ox-4
-    xend = ox+4
+for i in range(ymin,ymax):
+    xstart = ox-ticksize
+    xend = ox+ticksize
     yval = oy+i*dy
     line(xstart,yval,xend,yval)
     ylabel = str(i)
-    xtxt = ox+12
+    xtxt = ox+label_offset
     if i!=0:
         text(xtxt,yval,ylabel)
+
+
+# In[4]:
+
 
 # Create variables to monitor whether blank line has been entered, and the number of curves plotted
 looping = True
@@ -62,10 +94,9 @@ while (looping):
     setFill(col)
     setOutline(col)
     
-    # If a blank line was NOT entered, plot the curve
+    # If a blank line was NOT entered, plot the curve, as well as local maximum and minimum points
     if (looping):
-        step = 1
-        for xpixel in range(0,800,step):
+        for xpixel in range(xcs,xce,step):
             x = (xpixel-ox)/dx
             xstart = x*dx+ox
             y = eval(expression)
@@ -75,9 +106,51 @@ while (looping):
             y = eval(expression)
             yend = -y*dy+oy
             line(xstart,ystart,xend,yend)
+            
+            if (xpixel > xcs):
+                if (yend>ystart):
+                    compare = True
+                else:
+                    compare = False
+                if (compare != previous_step_increasing):
+                    #print ("max/min at x = ",xstart,ystart)
+                    if (compare == True):
+                            setFill("purple")
+                            setOutline("purple")
+                    else:
+                            setFill("orange")
+                            setOutline("orange")
+                    ellipse(xstart-ticksize,ystart-ticksize,2*ticksize,2*ticksize)
+                    setFill(col)
+                    setOutline(col)
+              
+            if (yend>ystart):
+                previous_step_increasing = True
+            else:
+                previous_step_increasing = False
     
     # Increment the number of plotted curves
     count = count + 1
 
 # Close the graphics window
 close()
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
